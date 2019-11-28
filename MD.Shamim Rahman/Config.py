744ad0cb8792de2af9dbe9764fdc90d06e0e33d6
MD.Shamim Rahman <43651007+Shamimrahman@users.noreply.github.com>
2019-11-28 22:43:51 +0600

    Update Config.py

--- MD.Shamim Rahman/Config.py
+++ MD.Shamim Rahman/Config.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from io import BytesIO
 
-subscription_key = "your-subscription-key"
-search_url = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
+subscription_key = "70e4e05c78354be3b0cb45ca5efb3460"
+search_url = "https://api.cognitive.microsoft.com/bing/v7.0/videos"
 search_term = "puppies"

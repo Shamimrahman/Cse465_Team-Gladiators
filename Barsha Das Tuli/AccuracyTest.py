1c9d724d40a3e29e4e8c07cf14527c5256c94639
BarshaDas <45540094+BarshaDas@users.noreply.github.com>
2019-12-23 03:55:03 +0600

    Create AccuracyTest.py

---
+++ Barsha Das Tuli/AccuracyTest.py
@@ -0,0 +1,51 @@
+import cv2     # for capturing videos
+import math   # for mathematical operations
+import matplotlib.pyplot as plt    # for plotting the images
+%matplotlib inline
+import pandas as pd
+from keras.preprocessing import image   # for preprocessing the images
+import numpy as np    # for mathematical operations
+from keras.utils import np_utils
+from skimage.transform import resize   # for resizing images
+
+count = 0
+videoFile = "harry.mp4"
+cap = cv2.VideoCapture(videoFile)   # capturing the video from the given path
+frameRate = cap.get(5) #frame rate
+x=1
+while(cap.isOpened()):
+    frameId = cap.get(1) #current frame number
+    ret, frame = cap.read()
+    if (ret != True):
+        break
+    if (frameId % math.floor(frameRate) == 0):
+        filename ="frame%d.jpg" % count;count+=1
+        cv2.imwrite(filename, frame)
+cap.release()
+print ("Done!")
+
+from keras.models import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.layers import Dense, InputLayer, Dropout
+
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))    # include_top=False to remove the top layer
+
+X_train = base_model.predict(X_train)
+X_valid = base_model.predict(X_valid)
+X_train.shape, X_valid.shape
+
+X_train = X_train.reshape(208, 7*7*512)      # converting to 1-D
+X_valid = X_valid.reshape(90, 7*7*512)
+
+train = X_train/X_train.max()      # centering the data
+X_valid = X_valid/X_train.max()
+Finally, we will build our model. This step cn be divided into 3 sub-steps:
+
+
+model = Sequential()
+model.add(InputLayer((7*7*512,)))    # input layer
+model.add(Dense(units=1024, activation='sigmoid')) # hidden layer
+model.add(Dense(3, activation='softmax'))    # output layer
+Let’s check the summary of the model using the summary() function:
+
+model.summary()

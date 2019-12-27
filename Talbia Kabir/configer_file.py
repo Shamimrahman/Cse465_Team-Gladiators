859a4f649fc5f4d0bb94269200d99df89473a6c9
Talbia Kabir <talbia165@gmail.com>
2019-12-28 03:39:23 +0600

    Delete configer_file.py

--- Talbia Kabir/configer_file.py
+++
@@ -1,9 +0,0 @@
-import requests
-import matplotlib.pyplot as plt
-from PIL import Image
-from io import BytesIO
-
-subscription_key = "70e4e05c78354be3b0cb45ca5efb3460"
-
-search_url = "https://api.cognitive.microsoft.com/bing/v7.0/videos"
-search_term = "puppies"
